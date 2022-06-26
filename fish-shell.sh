5a8fc87184c875c7d7d275b868ecd83dc0061a4d
Damien Boisvert <77273893+AlphaGameDeveloper@users.noreply.github.com>
2022-06-25 22:38:00 -0700

    Create fish-shell.sh

---
+++ fish-shell.sh
@@ -0,0 +1,10 @@
+clear
+echo "Fish Shell Install.
+Installing...
+"
+sudo apt install fish
+echo "All done;  Please run commands:
+  chsh -s /usr/bin/fish
+  sudo shsh -s /usr/bin/fish
+so Fish is your default shell.
+(It's much better!)
